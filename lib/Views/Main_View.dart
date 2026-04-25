52d1f5b78094afa146bea64747274debc34266f9
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-04-25 15:22:28 +0300

    Add custom font, enhance UI, and refactor code

--- lib/Views/Main_View.dart
+++ lib/Views/Main_View.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Widgets/Note_Card.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -25,46 +26,10 @@ class MainView extends StatelessWidget {
         elevation: 5,
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          height: 150,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.orange[200],
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                title: Text(
-                  "First Note",
-                  style: TextStyle(color: Colors.black, fontSize: 28),
-                ),
-                subtitle: Text(
-                  "This is  the first note in flutter app",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-                trailing: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.delete_rounded,
-                    color: Colors.black,
-                    size: 35,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  (DateTime.now().timeZoneOffset).toString(),
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: ListView(
+        children: [
+          NoteCard(),
+        ],
       ),
 
       floatingActionButton: Container(
