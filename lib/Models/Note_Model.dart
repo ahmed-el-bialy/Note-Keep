bcbf9ed553c296db7152440d6b28514328c08d11
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-04-25 20:11:52 +0300

    Setup Hive database for notes persistence

--- lib/Models/Note_Model.dart
+++ lib/Models/Note_Model.dart
@@ -1,8 +1,17 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'Note_Model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveType(typeId: 0)
   final String title;
+  @HiveType(typeId: 1)
   final String content;
-  final DateTime createdDate;
-  final DateTime lastEditedDate;
+  @HiveType(typeId: 2)
+  final String createdDate;
+  @HiveType(typeId: 3)
+  final String lastEditedDate;
+  @HiveType(typeId: 4)
   final int color;
 
   NoteModel({
