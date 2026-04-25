dc900ea282aab9bda39e7ebb34cae6d24e33d882
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-04-25 13:14:00 +0300

    first commit

---
+++ lib/Views/Main_View.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Keep Notes"),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search))
+        ],
+        elevation: 5,
+      ),
+
+      body: ListView(children: []),
+
+      floatingActionButton: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          color: Colors.lightBlueAccent,
+        ),
+        child: IconButton(
+          padding: EdgeInsets.all(15),
+          color: Colors.black,
+          onPressed: () {},
+          icon: Icon(Icons.add, size: 35),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_keep/Views/Main_View.dart';
+
+void main(){
+  runApp(NotesKeep());
+}
+
+
+class NotesKeep extends StatelessWidget {
+  const NotesKeep({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: MainView(),
+    );
+  }
+}
