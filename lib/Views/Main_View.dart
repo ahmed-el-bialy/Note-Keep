ec56583d4e1b7f6e3c9dd734bd64ac053fe2cf26
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-04-25 14:35:20 +0300

    Add static UI for Note Item widget and add some UI enhancements

--- lib/Views/Main_View.dart
+++ lib/Views/Main_View.dart
@@ -7,13 +7,13 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Keep Notes",style: TextStyle(fontSize: 27),),
+        title: Text("Keep Notes", style: TextStyle(fontSize: 27)),
         centerTitle: true,
         actions: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
             child: Card(
-              color: Colors.white.withOpacity(.1),
+              color: Colors.white.withValues(alpha: 0.1),
               elevation: 10,
               child: IconButton(
                 onPressed: () {},
@@ -23,6 +23,48 @@ class MainView extends StatelessWidget {
           ),
         ],
         elevation: 5,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 150,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.orange[200],
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  "First Note",
+                  style: TextStyle(color: Colors.black, fontSize: 28),
+                ),
+                subtitle: Text(
+                  "This is  the first note in flutter app",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.delete_rounded,
+                    color: Colors.black,
+                    size: 35,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  (DateTime.now().timeZoneOffset).toString(),
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
 
       floatingActionButton: Container(
